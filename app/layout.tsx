8d9903c4aced167b2ca9d150a8a6f68450b755b7
mvahaste <mvahaste@proton.me>
2025-02-24 23:40:02 +0200

    Bottom navigation bar, cleanup and format

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,8 @@ import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
-import MobileNavBar from "@/components/mobile-nav-bar";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
+import BottomNavigationBar from "@/components/mobile-nav-bar";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -35,39 +36,41 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col  items-center">
-              {/* Header */}
-              <header className="sticky top-0 z-50 flex h-16 w-full justify-center border-b border-b-foreground/10 bg-background/95 shadow-sm backdrop-blur-lg supports-[backdrop-filter]:bg-background/60">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <p className="font-medium text-lg">{metadata.title}</p>
-                  <nav className="flex flex-row gap-2 items-center">
-                    {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                  </nav>
+          <TooltipProvider>
+            <main className="min-h-screen flex flex-col items-center">
+              <div className="flex-1 w-full flex flex-col  items-center">
+                {/* Header */}
+                <header className="sticky top-0 z-50 flex h-16 w-full justify-center border-b border-b-foreground/10 bg-background/95 shadow-sm backdrop-blur-lg supports-[backdrop-filter]:bg-background/60">
+                  <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                    <p className="font-medium text-lg">{metadata.title}</p>
+                    <nav className="flex flex-row gap-2 items-center">
+                      {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                    </nav>
+                  </div>
+                </header>
+                {/* Content */}
+                <div className="flex flex-grow flex-col max-w-5xl p-5">
+                  {children}
                 </div>
-              </header>
-              {/* Content */}
-              <div className="flex flex-grow flex-col max-w-5xl p-5">
-                {children}
+                {/* Footer */}
+                {/* <footer className="w-full flex items-center justify-center  mx-auto text-center text-xs gap-8 py-6"> */}
+                {/*   <p> */}
+                {/*     View the source code on{" "} */}
+                {/*     <a */}
+                {/*       href="https://github.com/mvahaste/linkbook" */}
+                {/*       target="_blank" */}
+                {/*       className="font-bold hover:underline" */}
+                {/*       rel="noreferrer" */}
+                {/*     > */}
+                {/*       GitHub */}
+                {/*     </a> */}
+                {/*   </p> */}
+                {/* </footer> */}
+                {/* Mobile Nav Bar */}
+                <BottomNavigationBar />
               </div>
-              {/* Footer */}
-              <footer className="w-full flex items-center justify-center  mx-auto text-center text-xs gap-8 py-6">
-                <p>
-                  View the source code on{" "}
-                  <a
-                    href="https://github.com/mvahaste/linkbook"
-                    target="_blank"
-                    className="font-bold hover:underline"
-                    rel="noreferrer"
-                  >
-                    GitHub
-                  </a>
-                </p>
-              </footer>
-              {/* Mobile Nav Bar */}
-              <MobileNavBar />
-            </div>
-          </main>
+            </main>
+          </TooltipProvider>
         </ThemeProvider>
       </body>
     </html>
--- components/mobile-nav-bar.tsx
+++ components/mobile-nav-bar.tsx
@@ -2,49 +2,83 @@
 
 import {
   LucideBookmark,
-  LucidePlus,
   LucideSettings,
   LucideTag,
+  LucideIcon,
 } from "lucide-react";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { useCallback, useEffect } from "react";
 
-export default function MobileNavBar() {
+/**
+ * Navigation items.
+ */
+const navItems: {
+  key: string;
+  label: string;
+  href: string;
+  icon: LucideIcon;
+}[] = [
+  { key: "1", label: "Bookmarks", href: "/bookmarks", icon: LucideBookmark },
+  { key: "2", label: "Tags", href: "/tags", icon: LucideTag },
+  { key: "3", label: "Settings", href: "/settings", icon: LucideSettings },
+];
+
+export default function BottomNavigationBar() {
   const pathname = usePathname();
+  const router = useRouter();
+
+  /**
+   * Handle shortcut navigation.
+   */
+  const handleKeyPress = useCallback(
+    (event: KeyboardEvent) => {
+      const activeElement = document.activeElement;
+
+      if (
+        activeElement instanceof HTMLInputElement ||
+        activeElement instanceof HTMLTextAreaElement ||
+        (activeElement &&
+          activeElement.getAttribute("contenteditable") === "true")
+      ) {
+        return;
+      }
+
+      const item = navItems.find((item) => item.key === event.key);
+
+      if (item) {
+        router.push(item.href);
+      }
+    },
+    [router],
+  );
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [handleKeyPress]);
 
   return (
-    <div className="sticky bottom-0 z-50 h-16 flex w-full py-3 px-5 justify-between border-t border-t-foreground/10 bg-background/95 shadow-sm backdrop-blur-lg supports-[backdrop-filter]:bg-background/60 items-center">
-      {/* <Button className="rounded-full px-4" variant="ghost"> */}
-      {/*   <LucidePlus /> */}
-      {/* </Button> */}
-      <Button
-        className="rounded-full px-4"
-        asChild
-        variant={pathname == "/bookmarks" ? "default" : "ghost"}
-      >
-        <Link href="/bookmarks">
-          <LucideBookmark />
-        </Link>
-      </Button>
-      <Button
-        className="rounded-full px-4"
-        asChild
-        variant={pathname == "/tags" ? "default" : "ghost"}
-      >
-        <Link href="/tags">
-          <LucideTag />
-        </Link>
-      </Button>
-      <Button
-        className="rounded-full px-4"
-        asChild
-        variant={pathname == "/settings" ? "default" : "ghost"}
-      >
-        <Link href="/settings">
-          <LucideSettings />
-        </Link>
-      </Button>
+    <div className="sticky bottom-0 z-50 h-16 w-full py-3 px-6 border-t border-t-foreground/10 bg-background/95 shadow-sm backdrop-blur-lg supports-[backdrop-filter]:bg-background/60">
+      <div className="w-full max-w-lg flex flex-row items-center justify-between mx-auto">
+        {navItems.map(({ key, label, href, icon: Icon }) => (
+          <Button
+            key={key}
+            className="rounded-full px-4"
+            asChild
+            variant={pathname === href ? "default" : "ghost"}
+          >
+            <Link href={href} className="inline-flex gap-1 items-center">
+              <Icon />
+              <span className="hidden xs:inline">{label}</span>
+            </Link>
+          </Button>
+        ))}
+      </div>
     </div>
   );
 }
